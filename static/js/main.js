0d2f893dd1c4d9d9ad336a125a97ca57fb1b4763
pgm-jeflerou <jef.leroux@student.arteveldehs.be>
2022-01-26 13:24:50 +0100

    header done exept for large screens

--- static/js/main.js
+++ static/js/main.js
@@ -3,14 +3,35 @@
     initialize() {
       this.cacheElements();
       this.randomBackground();
+      this.registerListeners();
     },
 
     cacheElements() {
       this.$header_background = document.querySelector('.header_background');
+      this.$header_menu = document.querySelector('.header-menu');
+      this.$mobile_menu = document.querySelector('.mobile-menu');
+      this.$close_button = document.querySelector('.close');
+      this.$program_button = document.querySelector('.program');
+      this.$chevron = document.querySelector('.chevron')
+      this.$program_menu = document.querySelector('.program-menu-days');
     },
 
     registerListeners() {
+      this.$close_button.addEventListener('click', e => {
+        this.$mobile_menu.classList.toggle('shown');
+        this.$mobile_menu.classList.toggle('hidden');
+      }, false);
+      
+      this.$header_menu.addEventListener('click', e => {
+        this.$mobile_menu.classList.toggle('shown');
+        this.$mobile_menu.classList.toggle('hidden');
+      }, false);
 
+      this.$program_button.addEventListener('click', e => {
+        this.$program_menu.classList.toggle('shown');
+        this.$program_menu.classList.toggle('hidden');
+        this.$chevron.classList.toggle('rotate-180')
+      }, false);
     },
 
     randomBackground() {
